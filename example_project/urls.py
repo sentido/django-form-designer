7bbda5afe362cbf72a7d7ea16d4a7bae59e3514b
Plamen Nikolov <plamen.nikolov@sentido.bg>
2012-04-17 20:23:57 +0300

    Added example-project.

---
+++ example_project/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^forms/', include('form_designer.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    # Examples:
+    # url(r'^$', 'example_project.views.home', name='home'),
+    # url(r'^example_project/', include('example_project.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
